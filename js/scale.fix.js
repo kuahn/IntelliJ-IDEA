49a54526445af3358a69dbf5d9ea51e302153099
kuahn <z@kuahn.com>
2013-03-20 17:47:14 +0900

    first commit

--- js/scale.fix.js
+++ js/scale.fix.js
@@ -1,18 +1,25 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+(function () {
+    var metas = document.getElementsByTagName('meta')
+    ,   i;
+
+    function gestureStart() {
+        for (i=0; i<metas.length; i++) {
+            if (metas[i].name == "viewport") {
+                metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+            }
+        }
     }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
-  document.getElementById("tip").style.margin = '1% 2%';
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+
+    if (navigator.userAgent.match(/iPhone/i)) {
+        for (i=0; i<metas.length; i++) {
+            if (metas[i].name == "viewport") {
+                metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+            }
+        }
+        document.addEventListener("gesturestart", gestureStart, false);
+        document.getElementById("tip").style.margin = '1% 2%';
     }
-  }
-}
+}());
+
+
+
