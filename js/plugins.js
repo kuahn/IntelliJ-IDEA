c58adefe5f2a6f4c22e9cb67604b4f17b5ce3472
kuahn <z@kuahn.com>
2013-03-20 17:55:55 +0900

    first commit

--- js/plugins.js
+++ js/plugins.js
@@ -1,22 +1,45 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+    var metas = document.getElementsByTagName('meta')
+    ,   i
+    ,   method
+    ,   noop = function () {}
+    ,   methods = [
+            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+            'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+            'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+            'timeStamp', 'trace', 'warn'
+        ]
+    ,   length = methods.length
+    ,   console = (window.console = window.console || {});
 
-    while (length--) {
+    while ( length-- ) {
         method = methods[length];
 
         // Only stub undefined methods.
         if (!console[method]) {
             console[method] = noop;
         }
+    }
+
+    function gestureStart() {
+        for (i=0; i<metas.length; i++) {
+            if (metas[i].name == "viewport") {
+                metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+            }
+        }
+    }
+
+    if ( navigator.userAgent.match(/iPhone/i) ) {
+        for ( i=0; i<metas.length; i++ ) {
+            if (metas[i].name == "viewport") {
+                metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+            }
+        }
+
+        document.addEventListener("gesturestart", gestureStart, false);
+
+        // #tip adjust iphone size
+        document.getElementById("tip").style.margin = '1% 2%';
     }
 }());
