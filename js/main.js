26abe992537b74be2e5beda1240c19118208f630
kuahn <z@kuahn.com>
2013-03-22 00:50:17 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var tip = {
             url: "model/tip"+ num +".json",
             dataType: "json"
         }).done(function (data) {
-            var tip = data.tip || "Error"
+            var tip = data.tip+"<br/>" || "Error"
             ,   imgs = data.imgs || []
             ,   id = data.id || ""
             ,   conts = ""
