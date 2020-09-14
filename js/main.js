0419edf8bdb81322c61ed20cfafb897227bc2317
ahn.kwangun <ahn.kwangun@kakaocorp.com>
2020-09-14 22:25:19 +0900

    update img size

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ var Tip = {
                     nl = (img.nl === false ? "" : "<br/>");
                 }
 
-                conts += '<img src="'+ src +'" width="300"/>' + nl;
+                conts += '<img src="'+ src +'" width="500"/>' + nl;
             }
 
             if ( org ) {
